84e1e8cc2a2278f13ffdf69c4f0e7e104b4b2980
Anatoly Malyshev <anatoliym67@gmail.com>
2020-06-03 14:32:52 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,74 @@
+package ru.netology;
+
+public class Radio {
+    private int minRadioStation = 0;
+    private int maxRadioStation = 9;
+    private int currentRadioStation = 3;
+    private int minVolumeRadio = 0;
+    private int maxVolumeRadio = 10;
+    private int currentRadioVolume = 5;
+
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation < minRadioStation){
+            return;
+        }
+        if (currentRadioStation > maxRadioStation){
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public void nextRadioStation(){
+        if ((currentRadioStation + 1) <= maxRadioStation){
+            this.currentRadioStation = currentRadioStation + 1;
+        } else
+            this.currentRadioStation = 0;
+    }
+
+    public void previousRadioStation(){
+        if ((currentRadioStation - 1) >= minRadioStation){
+            this.currentRadioStation = currentRadioStation - 1;
+        } else
+            this.currentRadioStation = 9;
+    }
+
+    public int getCurrentRadioVolume() {
+        return currentRadioVolume;
+    }
+
+    public void increaseVolume(){
+        if (currentRadioVolume < maxVolumeRadio){
+            this.currentRadioVolume = currentRadioVolume + 1;
+        }
+        return;
+    }
+
+    public void decreaseVolume(){
+        if (currentRadioVolume > minVolumeRadio){
+            this.currentRadioVolume = currentRadioVolume - 1;
+        }
+        return;
+    }
+
+    public int getMinRadioStation() {
+        return minRadioStation;
+    }
+
+    public int getMaxRadioStation() {
+        return maxRadioStation;
+    }
+
+    public int getMinVolumeRadio() {
+        return minVolumeRadio;
+    }
+
+    public int getMaxVolumeRadio() {
+        return maxVolumeRadio;
+    }
+
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,82 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    public void shouldSetRadioChanel(){
+        radio.setCurrentRadioStation(3);
+        assertEquals(3,radio.getCurrentRadioStation());
+        System.out.println("Radio station is #" + radio.getCurrentRadioStation() + " now");
+    }
+
+    @Test
+    public void shouldntSetUnavailableStationNumber(){
+        radio.setCurrentRadioStation(15);
+        assertNotEquals(15,radio.getCurrentRadioStation());
+        System.out.println("Radio station is #" + radio.getCurrentRadioStation() + " now");
+    }
+
+    @Test
+    public void shouldntSetNegativeStationNumber(){
+        radio.setCurrentRadioStation(-7);
+        assertNotEquals(-7,radio.getCurrentRadioStation());
+        System.out.println("Radio station is #" + radio.getCurrentRadioStation() + " now");
+    }
+
+    @Test
+    public void shouldSwitchStationToBeginningAfterMax(){
+        System.out.println("shouldSwitchStationToBeginningAfterMax()");
+        for (int i = radio.getCurrentRadioStation(); i < (radio.getMaxRadioStation() + 3); i++){
+            radio.nextRadioStation();
+            System.out.println(radio.getCurrentRadioStation());
+        }
+    }
+
+    @Test
+    public void shouldSwitchStationToEndAfterMin(){
+        System.out.println("shouldSwitchStationToEndAfterMin()");
+        for (int i = radio.getCurrentRadioStation(); i > (radio.getMinRadioStation() - 3); i--){
+            radio.previousRadioStation();
+            System.out.println(radio.getCurrentRadioStation());
+        }
+    }
+
+    @Test
+    public void shouldIncreaseVolume(){
+        int previousVolumeLevel = radio.getCurrentRadioVolume() + 1;
+        radio.increaseVolume();
+        assertEquals(previousVolumeLevel,radio.getCurrentRadioVolume());
+        System.out.println("Volume level " + radio.getCurrentRadioVolume());
+    }
+
+    @Test
+    public void shouldDecreaseVolume(){
+        int previousVolumeLevel = radio.getCurrentRadioVolume() - 1;
+        radio.decreaseVolume();
+        assertEquals(previousVolumeLevel,radio.getCurrentRadioVolume());
+        System.out.println("Volume level " + radio.getCurrentRadioVolume());
+    }
+
+    @Test
+    public void shouldntSetVolumeHigherMax(){
+        System.out.println("shouldntSetVolumeHigherMax()");
+        for (int i = radio.getCurrentRadioVolume(); i < (radio.getMaxVolumeRadio() + 3); i++){
+            radio.increaseVolume();
+            System.out.println(radio.getCurrentRadioVolume());
+        }
+    }
+
+    @Test
+    public void shouldntSetVolumeLowerMin(){
+        System.out.println("shouldntSetVolumeLowerMin()");
+        for (int i = radio.getCurrentRadioVolume(); i > (radio.getMinVolumeRadio() - 3); i--){
+            radio.decreaseVolume();
+            System.out.println(radio.getCurrentRadioVolume());
+        }
+    }
+}
